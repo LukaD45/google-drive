58dd68e32821e43c67875fc50a8320d88d1ca44f
LukaD45 <luka.dusko44@gmail.com>
2025-02-04 14:32:18 +0100

    map over navItems and display them, responsivness

--- components/sidebar.tsx
+++ components/sidebar.tsx
@@ -1,7 +1,14 @@
+"use client";
+
+import { navItems } from "@/constants";
+import { cn } from "@/lib/utils";
 import Image from "next/image";
 import Link from "next/link";
+import { usePathname } from "next/navigation";
 
 const Sidebar = () => {
+  const pathname = usePathname();
+
   return (
     <aside className="sidebar">
       <Link href="/">
@@ -23,7 +30,30 @@ const Sidebar = () => {
       </Link>
 
       <nav className="sidebar-nav">
-        <ul className="flex flex-1 flex-col gap-6"></ul>
+        <ul className="flex flex-1 flex-col gap-6">
+          {navItems.map(({ url, name, icon }) => (
+            <Link key={name} href={url} className="lg:w-full ">
+              <li
+                className={cn(
+                  "sidebar-nav-item",
+                  pathname === url && "shad-active"
+                )}
+              >
+                <Image
+                  src={icon}
+                  alt={name}
+                  width={24}
+                  height={24}
+                  className={cn(
+                    "nav-icon",
+                    pathname === url && "nav-icon-active"
+                  )}
+                />
+                <p className="hidden lg:block">{name}</p>
+              </li>
+            </Link>
+          ))}
+        </ul>
       </nav>
     </aside>
   );
