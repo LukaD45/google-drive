1882769380291a038a40733c7cc48e522aa1c5d0
LukaD45 <luka.dusko44@gmail.com>
2025-02-04 14:23:42 +0100

    create navItems in constants folder

--- components/sidebar.tsx
+++ components/sidebar.tsx
@@ -1,5 +1,32 @@
+import Image from "next/image";
+import Link from "next/link";
+
 const Sidebar = () => {
-  return <div>Sidebar</div>;
+  return (
+    <aside className="sidebar">
+      <Link href="/">
+        <Image
+          src="/assets/icons/logo-full-brand.svg"
+          alt="logo"
+          width={160}
+          height={150}
+          className="hidden h-auto lg:block"
+        />
+
+        <Image
+          src="/assets/icons/logo-brand.svg"
+          alt="logo"
+          width={52}
+          height={52}
+          className="h-auto lg:hidden"
+        />
+      </Link>
+
+      <nav className="sidebar-nav">
+        <ul className="flex flex-1 flex-col gap-6"></ul>
+      </nav>
+    </aside>
+  );
 };
 
 export default Sidebar;
