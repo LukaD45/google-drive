5d0656a264ba0f9abfc795719350dc25adc44f15
LukaD45 <luka.dusko44@gmail.com>
2025-02-05 20:52:01 +0100

    create createQueries function

--- lib/actions/file.actions.ts
+++ lib/actions/file.actions.ts
@@ -3,9 +3,10 @@
 import { createAdminClient } from "../appwrite";
 import { InputFile } from "node-appwrite/file";
 import { appwriteConfig } from "../appwrite/config";
-import { ID } from "node-appwrite";
+import { ID, Query } from "node-appwrite";
 import { constructFileUrl, getFileType, parseStringify } from "../utils";
 import { revalidatePath } from "next/cache";
+import { getCurrentUser } from "./user.actions";
 
 const handleError = (error: unknown, message: string) => {
   console.log(error, message);
@@ -57,4 +58,22 @@ export const uploadFile = async ({
   } catch (error) {
     handleError(error, "Failed to upload file");
   }
+};
+
+const createQueries = (currentUser: any) => {
+    const queries=[
+        Query.or([Query.equal("owner",[currentUser.$id]),Query.equal("users",[currentUser.email])])
+    ]
+
+export const getFiles = async () => {
+  const databases = await createAdminClient();
+  try{
+   const currentUser = await getCurrentUser();
+
+   if(!currentUser) throw new Error("User not found");
+
+   const queries=createQueries(currentUser)
+    
+  }catch(error){
+    handleError(error, "Failed to get files");
 };
