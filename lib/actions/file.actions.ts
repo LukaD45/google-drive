0eac77eb8f8cde942f9fe64e42bf090f91dab481
LukaD45 <luka.dusko44@gmail.com>
2025-02-10 20:50:53 +0100

    create logic for sort and search

--- lib/actions/file.actions.ts
+++ lib/actions/file.actions.ts
@@ -61,10 +61,17 @@ export const uploadFile = async ({
   }
 };
 
-const createQueries = (currentUser: Models.Document, types: string[]) => {
+const createQueries = (
+  currentUser: Models.Document,
+  types: string[],
+  searchText: string,
+  sort: string,
+  limit?: number
+) => {
   const queries = [
     Query.or([
       Query.equal("owner", [currentUser.$id]),
+
       Query.contains("users", [currentUser.email]),
     ]),
   ];
@@ -72,18 +79,39 @@ const createQueries = (currentUser: Models.Document, types: string[]) => {
   if (types.length > 0) {
     queries.push(Query.equal("type", types));
   }
+  if (searchText.length > 0) {
+    queries.push(Query.contains("name", searchText));
+  }
+
+  if (limit) {
+    queries.push(Query.limit(limit));
+  }
+
+  if (sort) {
+    const [sortBy, orderBy] = sort.split("-");
+    queries.push(
+      orderBy === "asc" ? Query.orderAsc(sortBy) : Query.orderDesc(sortBy)
+    );
+  }
+
+  console.log(queries);
 
   return queries;
 };
 
-export const getFiles = async ({ types = [] }: GetFilesProps) => {
+export const getFiles = async ({
+  types = [],
+  searchText = "",
+  sort = "$createdAt-desc",
+  limit,
+}: GetFilesProps) => {
   const { databases } = await createAdminClient();
   try {
     const currentUser = await getCurrentUser();
 
     if (!currentUser) throw new Error("User not found");
 
-    const queries = createQueries(currentUser, types);
+    const queries = createQueries(currentUser, types, searchText, sort, limit);
 
     console.log({ currentUser, queries });
 
