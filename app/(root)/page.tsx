a7ba0c5e527999aec06419951e65aaa848c6cc25
LukaD45 <luka.dusko44@gmail.com>
2025-02-12 07:39:30 +0100

    create getTotalSpaceUsed function

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,4 +1,4 @@
-export default function Home() {
+export default async function Dashboard() {
   return (
     <div className="flex-center h-screen">
       <h1 className="h1">StoreIt - the only storage you need</h1>
